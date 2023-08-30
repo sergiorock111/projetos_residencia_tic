2105e82c2bf16edc884481caa9c2c05351c1532f
sérgio de souza santos <https://github.com/sergiorock111/projetos_residencia_tic.git>
2023-08-30 18:57:58 -0300

    first commit

---
+++ meuprog1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std ;
+
+int main (void){
+
+string nome_do_usuario;
+cout << "Digite seu nome: ";
+cin >> nome_do_usuario ;
+cout << "bom dia!! " << nome_do_usuario;
+return 0; 
+
+
+}
+
